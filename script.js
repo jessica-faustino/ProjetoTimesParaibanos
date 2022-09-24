c517cc9969cad151722dd1ac77dfad4b40111594
Jessica Faustino <jessicacfaustino@gmail.com>
2022-09-24 14:03:49 -0300

    Finalizando a semana 6

--- script.js
+++ script.js
@@ -31,56 +31,64 @@ const filme2 = {
     continuacao: true,
     elenco: ["Johnny Depp", "Keira Knightley", "Orlando Bloom", "Geoffrey Rush", "Kevin McNally"]
  }
+ const filme6 = {titulo: 'O Auto da Compadecida',
+duracao: 2000,
+continuacao: false,
+elenco: 'Matheus Nachtergaele, Selton Mello, Marco Nanini, Rogério Cardoso, Diogo Villela, Denise Fraga, Lima Duarte, Luís Melo,Fernanda Montenegro'
+}
+
+ 
 
 const filmes = [];
 
-// SEMANA 3
+// ************************* SEMANA 3 *******************************
 //Adicionando novos objetos no array de objetos, utilizando o push()
 
 //filmes.push(filme1,filme2,filme3,filme4);
 
+// ************************ SEMANA 4 **********************************
 //Fazendo uma condicional antes de adicionar os objetos ao array SEMANA 4
 
 if (filme1.continuacao === true){
     filmes.push(filme1);
-}
-else {
-    console.log("**ALERT** '" + filme1.titulo +"' não foi adicionado");
-}    
+}else {
+    console.log("**ALERT** '" + filme1.titulo +"' não foi adicionado");} 
+
 if (filme2.continuacao === true){
     filmes.push(filme2)
-}
-else {
-    console.log("**ALERT** '" + filme2.titulo +"' não foi adicionado");
-}
+}else {
+    console.log("**ALERT** '" + filme2.titulo +"' não foi adicionado");}
+
 if (filme3.continuacao === true){
     filmes.push(filme3)
-}
-else{ 
-    console.log("**ALERT** '" + filme3.titulo +"' não foi adicionado");
-}
+}else{ 
+    console.log("**ALERT** '" + filme3.titulo +"' não foi adicionado");}
+
 if (filme4.continuacao=== true){
     filmes.push(filme4);
-}
-else{
-    console.log("**ALERT**'" + filme4.titulo +"' não foi adicionado");
-}
-if (filme5.continuacao=== true){
-    filmes.push(filme5);
-}
-else{
-    console.log("**ALERT**'" + filme5.titulo +"' não foi adicionado");
-}
+}else{
+    console.log("**ALERT**'" + filme4.titulo +"' não foi adicionado");}
 
+if (filme5.continuacao === true){
+    filmes.push(filme5);
+}else{
+    console.log("**ALERT**'" + filme5.titulo +"' não foi adicionado");}
+
+if (filme6.continuacao=== true){
+    filmes.push(filme6);
+}else{
+    console.log("**ALERT**'" + filme6.titulo +"' não foi adicionado");}
 
 console.log ("Filmes que tem continuação: ", filmes);
 
-// SEMANA 5
+
+
+// ***************************** SEMANA 5 *********************************
 // Fazendo um laço para ler o array do objeto como string
 
 let auxiliar = "";
 
-// for(let i = 0; i < mes.lefilngth; i++){
+// for(let i = 0; i < filmes.length; i++){
         
 //     for(let j = 0; j < 1; j++){
 //         auxiliar += `Elenco: ${filmes[i].elenco.toString()} \n`
@@ -97,41 +105,69 @@ for (i in filmes){
     }
 }
 
-console.log(auxiliar)
+//console.log(auxiliar)
 
-/*
-const media = (duracao1 + duracao2 + duracao3 + duracao4)/3;
-console.log (`Duração média dos filmes ${media.toFixed(2)} minutos.`);
+const todosOsFilmes = [];
+todosOsFilmes.push(filme1,filme2,filme3,filme4,filme5)
 
-const continuacao = continuacao1 && continuacao2 && continuacao3 && continuacao4;
-console.log (continuacao);
-
-console.log (`Filme: ${filme1.toUpperCase()}
-Duração: ${duracao1}
-Tem continuação? ${continuacao1}
-Elenco: ${elenco1}`);
-
-console.log (`Filme: ${filme2.toUpperCase()}
-Duração: ${duracao2}
-Tem continuação? ${continuacao2}
-Elenco: ${elenco2}`);
-
-console.log (`Filme: ${filme3.toUpperCase()}
-Duração: ${duracao3}
-Tem continuação? ${continuacao3}
-Elenco: ${elenco3}`);
-
-console.log (`Filme: ${filme4.toUpperCase()}
-Duração: ${duracao4}
-Tem continuação? ${continuacao4}
-Elenco: ${elenco4}`); */
-
-/* const filmes = [filme1,filme2,filme3,filme4];
+//console.log (todosOsFilmes)
 
 
-for ( const i=0; i <= filmes.length; i++) {
-    
-    console.log (filmes[i]);
+// ********************* SEMANA 6 ***********************
+//Recebendo um objeto e devolvendo uma String do relatório com os dados do objeto.
+
+const devolvendoString = (objeto) => {
+    var texto = JSON.stringify(objeto,null,2);
+    return texto;
 }
 
-console.log (filmes[i]) */
+//console.log (devolvendoString(filmes));
+
+//SEMANA 6
+//1. Crie uma função que receba como parâmetro um objeto, e devolva a string do relatório com os dados do objeto.
+
+function imprimeString (objeto) { //IMPRIME UM ÚNICO OBJETO
+    
+    console.log(`Informações sobre o filme: ${objeto.titulo} \n Com duração de: ${objeto.duracao} \n Possui continuação: ${objeto.continuacao} \n Elenco: ${objeto.elenco}`);
+
+}
+
+//imprimeString(filme3)
+
+function imprimeStringObjeto(objetos){ //IMPRIME A LISTA DE OBJETOS DO ARRAY
+    for(let string of objetos){
+        imprimeString(string)
+    }
+}
+
+imprimeStringObjeto(filmes);
+
+//2. Crie uma função que recebe um array de objetos e uma string. Esta função deve retornar um objeto, e o objeto retornado deve possuir apenas os itens que tenham o nome/título igual à string passada como parâmetro. Caso não exista um item, exiba um ALERT indicando que nenhum item foi encontrado.
+
+//criar função com dois parametros, array de objetos e string
+//função retorna objeto
+//objeto precisa ter vaLor igual ao parametro passado como string //USAR FILTER?
+//caso não tenha valor igual ao parametro, rodar alerta
+
+function arrayString(arrayDeObjetos, nomeDoFilme){
+    const resultado = arrayDeObjetos.filter(movie => {
+        return movie.titulo.toUpperCase().includes(nomeDoFilme.toUpperCase())
+    })
+    if (resultado.length === 0){
+        alert("Filme não encontrado!")
+    }else{
+    return resultado
+    }
+}
+
+const nomeDoFilme = prompt("Digite aqui um filme dos Studios Ghibli:");
+
+console.log(arrayString (filmes, nomeDoFilme))
+
+// let filtroFilmes = arrayString(filmes, nomeDoFilme)
+// imprimeStringObjeto(filtroFilmes)
+
+
+
+
+
