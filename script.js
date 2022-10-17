eb8d8ed75063edffa234cf18dcc953e7de20d93d
Jessica Faustino <jessicacfaustino@gmail.com>
2022-10-16 23:58:23 -0300

    Mudança total no estilo do projeto

--- script.js
+++ script.js
@@ -1,89 +1,108 @@
-const filme1 = {
-    titulo : "As Branquelas",
-    duracao : 109,
-    continuacao : false, 
-    elenco : ["Shawn Wayans", "Marlon Wayans", "Maitland Ward", "Anne Dudek", "Terry Crews"],
-    capa: ""
+const time1 = {
+    titulo: "CAMPINENSE CLUBE",
+    fundacao: 1915,
+    campeaoParaibano: true,
+    elenco: ["Goleiros: Mauro Iguatu, Camilo e Zé Arthur.", "Zagueiros: Bremer Carvalho, Cleiton, Michel Bennech e Mateus Santana.", "Laterais: André Mascena, Émerson, Filipe Ramon, Biel, Patryck Annes e Oliveira.", "Volantes: Magno, Jeserson Lima, Lucas Alisson e PAtrick.", "Meias: Dione, Juninho, Gui Campana e Eduardo.", "Atacantes: Alan, Caíque, Carlos Maia, Erik Pulga, Pedro Chinês, Hugo Freitas e Rodrigo Fumaça."],
+    brasao: "./Escudo/Campinense.png",
+     link: "https://campinenseclube1915.com.br",
+    hino: "./Hinos/Hino Oficial do Campinense Clube - PB.mp4",
+    historia: "<b>História:</b> O Campinense Clube é uma agremiação esportiva de Campina Grande, no estado da Paraíba, fundada em 12 de abril de 1915, por 29 pessoas. Possui 22 títulos do Campeonato Paraibano, detendo a maior sequência de títulos do estado – hexacampeão entre 1960 e 1965.",
 }
 
-const filme2 = {
-    titulo: "Todo mundo em pânico",
-    duracao: 88,
-    continuacao : true,
-    elenco : ["Anna Faris", "Shawn Wayans", "Marlon Wayans", "Shannon Elizabeth", "Regina Hall"],
-    capa: ""
+const time2 = {
+    titulo: "TREZE FUTEBOL CLUBE",
+    fundacao: 1925,
+    campeaoParaibano: true,
+    elenco: ["Goleiros: Luciano e Andrezon.", "Zagueiros: Patrick, Hyago de Oliveira e Eduardo Elias.", "Laterais: Guilherme Rocha, Sanderson Cardoso, Ikaro e Carlo Antônio.", "Volantes: Lucas, Olivio, Raphael Augusto, Jean Sergipano, João Victor e Marcelo Barbosa.", "Meias: David Silva.", "Atacantes: Marcos Vinicius, José Vieira, Ramon Soares, Werveton e Matheus Totti."],
+    brasao: "./Escudo/logo-treze-de-campina-grande-2048.png",
+    link: "https://trezefc.com.br",
+    hino: "./Hinos/HINO DO TREZE-PB.mp3",
+    historia: "<b>História:</b> O Treze Futebol Clube, conhecido como o Galo da Borborema, é uma agremiação poliesportiva brasileira com sede na cidade de Campina Grande, Paraíba. Fundado em 7 de setembro de 1925 no centro da cidade, mudando-se para o bairro do São José na década de 1940. "
+
 }
 
- const filme3 = {
-    titulo: "O dia em que a Terra parou",
-    duracao : 104,
-    continuacao : false,
-    elenco : ["Keanu Reeves", "Jennifer Connelly", "Patricia Neal", "Jaden Smith", "Jon Hamm"],
-    capa: ""
+const time3 = {
+    titulo: "BOTAFOGO FUTEBOL CLUBE",
+    fundacao: 1931,
+    campeaoParaibano: true,
+    elenco: ["Goleiros: Luís Carlos, Lucas Ferreira e Edilson", "Lateral: Sávio, Alessandro, Bruno Ré e Lucas Gabriel.", "Zagueiro: Grabriel Yanno, Leandro Camilo, Paulo Vitor.", "Meio-Campo: Pablo, Ratinho, Adriano Junior, Esquerdinha e Nadson", "Atacante: Gustavo Coutinho, Nicolas, Leilson, Adilson Bahia e Rafael Barros"],
+    brasao: "./Escudo/botafogo.png",
+    link: "https://botafogopb.com.br",
+    hino: "./Hinos/03 Hino (Versão Original).mp3",
+    historia: "<b>História:</b> Botafogo Futebol Clube, mais conhecido como Botafogo da Paraíba, é um clube de futebol brasileiro da cidade de João Pessoa. Fundado no dia 28 de setembro de 1931, é um dos times mais tradicionais do estado da Paraíba."
 }
 
- const filme4 = { 
-    titulo: "Vingadores",
-    duracao : 143,
-    continuacao : true,
-    elenco : ["Robert Downey Jr.", "Scarlett Johansson", "Chris Evans", "Mark Ruffalo", "Chris Hemsworth", "Tom Hiddleston"],
-    capa: ""
+const time4 = {
+    titulo: "NACIONAL ATLÉTICO CLUBE",
+    fundacao: 1961,
+    campeaoParaibano: true,
+    elenco: ["Goleiros: Jean e Damião.", "Zagueiros: Arlan, Rafael Henriques, Keven e Antoniel.", "Laterais: Denis, Arthurzinho e Jhonatan.", "Volantes: Cleyton, Laércio, João Marcelo e Daniel.", "Meias: Romarinho, Clebinho e Deda.", "Atacantes: Felipe Araújo, Bidoga, Du e Pedrão"],
+    brasao: "./Escudo/nacional-de-patos-pb.png",
+    link: "http://www.instagram.com/nacionaldepatosoficial",
+    hino: "./Hinos/Hino do Nacional Atlético Clube ( Patos - PB ).mp4",
+    historia: "<b>História:</b>Foi fundado por funcionários federais da cidade, notadamente dos Correios e Telégrafos. Suas cores iniciais eram o verde e o amarelo, posteriormente mudando para os atuais verde e branco, devido então às normas da CBF."
 }
- const filme5 = {
-    titulo: "Piratas do Caribe, a maldição do pérola negra",
-    duracao: 143,
-    continuacao: true,
-    elenco: ["Johnny Depp", "Keira Knightley", "Orlando Bloom", "Geoffrey Rush", "Kevin McNally"],
-    capa: ""
- }
- const filme6 = {titulo: 'O Auto da Compadecida',
-duracao: 2000,
-continuacao: false,
-elenco: 'Matheus Nachtergaele, Selton Mello, Marco Nanini, Rogério Cardoso, Diogo Villela, Denise Fraga, Lima Duarte, Luís Melo,Fernanda Montenegro',
-capa: ""
+const time5 = {
+    titulo: "SOUZA ESPORTE CLUBE",
+    fundacao: 143,
+    campeaoParaibano: true,
+    elenco: ["Goleiros: Ricardo, Júnior Conceição, Lucas gabriel e João Vitor", "Laterias: André Victor, Gustavo, Danilo Itaporanga e Iranilson.", "Volantes: Romeu, Gleidson e Marlinho.", "Meias: Juninho, Daniel Costa, Keyllo, João Victor e Esquerdinha.", "Atacantes: Piauí"],
+    brasao: "./Escudo/SousaEC.png",
+    link: "https://www.instagram.com/sousa_ec",
+    hino: "./Hinos/03 Hino (Versão Original).mp3",
+    historia: "<b>História:</b>Fundado em 10 de julho de 1991 (mesmo dia do aniversário da cidade de Sousa), o clube começou a competir profissionalmente na Série B estadual do mesmo ano, sagrando-se campeão do certame. "
+}
+const time6 = {
+    titulo: "ATLÉTICO DE CAJAZEIRAS",
+    fundacao: 2000,
+    campeaoParaibano: true,
+    elenco: ["Goleiros: Saulo, Márcio Victor, Danilo, Gabriel e Ismael.", "Zagueiros: Lucas Caldas, Blanthon, Jan Pieter, Léo Alves e Gean.", "Volantes: Marcílio, Adrian, Russo, Romerito, Douglas Palestina e kaio Mateus.", "Laterais: Tata, Batata, Neto, Denis e Edmilson.", "Meias: Jorge, Matheus, Ramon, Vitor, Renatinho, Kiko Alagoano e Sorín.", "Atacantes: Ronda e Roger."],
+    brasao: "./Escudo/AtleticoCajazeirenseD.png",
+    link: "https://www.instagram.com/atleticooficialcz",
+    hino: "./Hinos/Hino do Atlético Cajazeirense de Desportos ( PB ) _ Oficial.mp4",
+    historia: "<b>História:</b> O Atlético Cajazeirense de Desportos ou mais conhecido por Atlético de Cajazeiras é uma agremiação esportiva de Cajazeiras, no estado da Paraíba, criada em 3 de julho de 1948. É o único time de Cajazeiras a ser campeão do Campeonato Paraibano e o com mais presenças."
 }
 
-const filmes = [];
+const times = [];
 
-// ************************* SEMANA 3 *******************************
-//Adicionando novos objetos no array de objetos, utilizando o push()
 
-//filmes.push(filme1,filme2,filme3,filme4);
+if (time1.campeaoParaibano === true) {
+    times.push(time1);
+} else {
+    console.log("**ALERT** '" + time1.titulo + "' não foi adicionado");
+}
 
-// ************************ SEMANA 4 **********************************
-//Fazendo uma condicional antes de adicionar os objetos ao array SEMANA 4
+if (time2.campeaoParaibano === true) {
+    times.push(time2)
+} else {
+    console.log("**ALERT** '" + time2.titulo + "' não foi adicionado");
+}
 
-if (filme1.continuacao === true){
-    filmes.push(filme1);
-}else {
-    console.log("**ALERT** '" + filme1.titulo +"' não foi adicionado");} 
+if (time3.campeaoParaibano === true) {
+    times.push(time3)
+} else {
+    console.log("**ALERT** '" + time3.titulo + "' não foi adicionado");
+}
 
-if (filme2.continuacao === true){
-    filmes.push(filme2)
-}else {
-    console.log("**ALERT** '" + filme2.titulo +"' não foi adicionado");}
+if (time4.campeaoParaibano === true) {
+    times.push(time4);
+} else {
+    console.log("**ALERT**'" + time4.titulo + "' não foi adicionado");
+}
 
-if (filme3.continuacao === true){
-    filmes.push(filme3)
-}else{ 
-    console.log("**ALERT** '" + filme3.titulo +"' não foi adicionado");}
+if (time5.campeaoParaibano === true) {
+    times.push(time5);
+} else {
+    console.log("**ALERT**'" + time5.titulo + "' não foi adicionado");
+}
 
-if (filme4.continuacao=== true){
-    filmes.push(filme4);
-}else{
-    console.log("**ALERT**'" + filme4.titulo +"' não foi adicionado");}
+if (time6.campeaoParaibano === true) {
+    times.push(time6);
+} else {
+    console.log("**ALERT**'" + time6.titulo + "' não foi adicionado");
+}
 
-if (filme5.continuacao === true){
-    filmes.push(filme5);
-}else{
-    console.log("**ALERT**'" + filme5.titulo +"' não foi adicionado");}
-
-if (filme6.continuacao=== true){
-    filmes.push(filme6);
-}else{
-    console.log("**ALERT**'" + filme6.titulo +"' não foi adicionado");}
-
-console.log ("Filmes que tem continuação: ", filmes);
+console.log("Times que conquistaram o Campeonato Paraíbano: ", times);
 
 
 
@@ -92,59 +111,66 @@ console.log ("Filmes que tem continuação: ", filmes);
 
 let auxiliar = "";
 
-// for(let i = 0; i < filmes.length; i++){
-        
+// for(let i = 0; i < times.length; i++){
+
 //     for(let j = 0; j < 1; j++){
-//         auxiliar += `Elenco: ${filmes[i].elenco.toString()} \n`
+//         auxiliar += `Elenco: ${times[i].elenco.toString()} \n`
 //     }
-    
-//     auxiliar += `Título: ${filmes[i].titulo} \nDuração: ${filmes[i].duracao} \nContinuação: ${filmes[i].continuacao} \n\n`
+
+//     auxiliar += `Título: ${times[i].titulo} \nDuração: ${times[i].fundacao} \nContinuação: ${times[i].fundacao} \n\n`
 // }
 
-for (i in filmes){
-    auxiliar += `Título: ${filmes[i].titulo} \nDuração: ${filmes[i].duracao} \nContinuação: ${filmes[i].continuacao} \n`
-    
-    for(j = 0; j < 1; j++){
-        auxiliar += `Elenco: ${filmes[i].elenco.toString()} \n\n`
+for (i in times) {
+    auxiliar += `Título: ${times[i].titulo} \n Fundado em: ${times[i].fundacao} \n Campeão Paraibano: ${times[i].fundacao} \n`
+
+    for (j = 0; j < 1; j++) {
+        auxiliar += `Elenco: ${times[i].elenco.toString()} \n\n`
     }
 }
 
 //console.log(auxiliar)
-
-const todosOsFilmes = [];
-todosOsFilmes.push(filme1,filme2,filme3,filme4,filme5,filme6)
-
-//console.log (todosOsFilmes)
 
 
 // ********************* SEMANA 6 ***********************
 //Recebendo um objeto e devolvendo uma String do relatório com os dados do objeto.
 
 const devolvendoString = (objeto) => {
-    var texto = JSON.stringify(objeto,null,2);
+    var texto = JSON.stringify(objeto, null, 2);
     return texto;
 }
 
-//console.log (devolvendoString(filmes));
+// console.log (devolvendoString(times));
 
 //SEMANA 6
 //1. Crie uma função que receba como parâmetro um objeto, e devolva a string do relatório com os dados do objeto.
 
-function imprimeString (objeto) { //IMPRIME UM ÚNICO OBJETO
-    
-    console.log(`Informações sobre o filme: ${objeto.titulo} \n Com duração de: ${objeto.duracao} \n Possui continuação: ${objeto.continuacao} \n Elenco: ${objeto.elenco}`);
+function imprimeString(objeto) { //IMPRIME UM ÚNICO OBJETO
+
+    console.log(`* ${objeto.titulo} * \n Fundado em: ${objeto.fundacao} \n Campeão Paraibano: ${objeto.fundacao} \n Elenco: ${objeto.elenco}`);
 
 }
 
-//imprimeString(filme3)
+imprimeString(time3)
 
-function imprimeStringObjeto(objetos){ //IMPRIME A LISTA DE OBJETOS DO ARRAY
-    for(let string of objetos){
+function imprimeStringObjeto(objetos) { //IMPRIME A LISTA DE OBJETOS DO ARRAY
+    for (let string of objetos) {
         imprimeString(string)
     }
 }
 
-imprimeStringObjeto(filmes);
+imprimeStringObjeto(times);
+
+//FUNÇÃO DE PESQUISA
+const funcaoBusca = (array, input) => {
+    for (i in array){
+        for(j in array[i]){
+            let section = document.getElementById("container")
+        }
+    }
+}
+
+
+
 
 //2. Crie uma função que recebe um array de objetos e uma string. Esta função deve retornar um objeto, e o objeto retornado deve possuir apenas os itens que tenham o nome/título igual à string passada como parâmetro. Caso não exista um item, exiba um ALERT indicando que nenhum item foi encontrado.
 
@@ -153,25 +179,58 @@ imprimeStringObjeto(filmes);
 //objeto precisa ter vaLor igual ao parametro passado como string 
 //caso não tenha valor igual ao parametro, rodar alerta
 
-function arrayString(arrayDeObjetos, nomeDoFilme){
-    const resultado = arrayDeObjetos.filter(movie => {
-        return movie.titulo.toUpperCase().includes(nomeDoFilme.toUpperCase())
-    })
-    if (resultado.length === 0){
-        alert("Filme não encontrado!")
-    }else{
-    return resultado
+let nomeDoTime = "Campinense Clube"
+let nomeDoTime1 = nomeDoTime.toUpperCase();
+
+function retornaObjeto (arrayObj,string){
+    let retornaObjeto = {};
+    for (let i in arrayObj){
+        for(let j in arrayObj[i]){
+            if (arrayObj[i][j]=== string){
+                retornaObjeto = arrayObj[i]
+            }
+        }
     }
+    console.log(retornaObjeto);
 }
 
-const nomeDoFilme = prompt("Digite aqui um filme:");
-
-console.log(arrayString (todosOsFilmes, nomeDoFilme))
-
-let filtroFilmes = arrayString(todosOsFilmes, nomeDoFilme)
-imprimeStringObjeto(filtroFilmes);
+retornaObjeto(times, nomeDoTime1);
 
 
+let variavelTemporaria = ''
 
+elementoDoDom = document.querySelector('.suaLista')
+function imprimirTodos() {
+    for (i in times) {
+       variavelTemporaria += `<article class='item' id='time${Number(i)+1}'>
+                <li>
+                    <a href='${times[i].link}'>
+                        <h3><button class='botaoSite'>${times[i].titulo}</button></h3>
+                    </a>
+                    <img src='${times[i].brasao}' width='25%' alt='Brasão do time'><br><br>
+                    <b>Fundação:</b> ${times[i].fundacao} <br><br>
+                    <b>Campeão Paraibano:</b> ${times[i].campeaoParaibano} <br><br>
+                    <b>Elenco:</b> ${times[i].elenco} <br><br>
+                    <b>História:</b> ${times[i].historia} <br><br><br>
+                    <audio src='${times[i].hino}' controls></audio><br><br>
+   
+    
+                </li>
+            </article>`
+    }
+    return elementoDoDom.innerHTML = variavelTemporaria
+}
 
+imprimirTodos()
 
+function busca(event){
+    event.preventDefault()
+    let input = document.getElementById("input").value.
+    toUpperCase()
+    if(input === ""){
+        alert("Este objeto não existe!")
+    }else{
+        document.getElementById("suaLista").remove()
+        retornaObjeto(times, input);
+    }
+} 
